1591b8318ec0a576cdadbafd09db43298f66dc30
Filippannilsson <filippanilsson95@gmail.com>
2024-10-24 11:06:10 +0200

    Påbörjat code-behind till AddWorkoutWindow

--- ProjektuppgiftOPG/ViewModel/AddWorkoutWindowViewModel.cs
+++ ProjektuppgiftOPG/ViewModel/AddWorkoutWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjektuppgiftOPG.MVVM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,63 @@ using System.Threading.Tasks;
 
 namespace ProjektuppgiftOPG.ViewModel
 {
-    public class AddWorkoutWindowViewModel
+    public class AddWorkoutWindowViewModel : ViewModelBase
     {
+        //Egenskaper
+        private DateTime dateInput;
+        public DateTime DateInput
+        {
+            get { return dateInput; }
+            set
+            {
+                dateInput = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string selectedType;
+        public string SelectedType
+        {
+            get { return selectedType; }
+            set
+            {
+                selectedType = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string durationInput;
+        public string DurationInput
+        {
+            get { return durationInput; }
+            set
+            {
+                durationInput = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string caloriesInput;
+        public string CaloriesInput
+        {
+            get { return caloriesInput; }
+            set
+            {
+                caloriesInput = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string notesInput;
+        public string NotesInput
+        {
+            get { return notesInput; }
+            set
+            {
+                notesInput = value;
+                OnPropertyChanged();
+            }
+        }
+
     }
 }
